78a7d0b77ffd93e732b472da6b5da49ced2a3db7
Aquahulk <abhisheksingh04062001@gmail.com>
2025-02-02 02:29:48 +0530

    Fixed circular imports

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SECRET_KEY'] = 'e187eb5382610cda775c8679351720cc143400548c174050cea779eec6d3f811'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     
     # Initialize extensions with the app
@@ -24,3 +24,9 @@ def create_app():
         db.create_all()
     
     return app
+
+# Create the app instance
+app = create_app()
+
+
+
